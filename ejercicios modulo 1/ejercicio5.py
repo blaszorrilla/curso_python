a563f3a3d8921148cb8f609ea17e8ec104d1f573
blaszorrilla <blassalvador03@gmail.com>
2023-09-12 16:24:12 -0400

    Calcular el promedio de notas

---
+++ ejercicios modulo 1/ejercicio5.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Ingrese la nota 1: "))
+nota2 = float(input("Ingrese la nota 2: "))
+nota3 = float(input("Ingrese la nota 3: "))
+nota4 = float(input("Ingrese la nota 4: "))
+
+promedio = (nota1+nota2+nota3+nota4)/4
+print(f"El promedio de las 4 notas es: {promedio:.2f}")
