bbe51443a60c8bdf7458670c98fc03ebbb8f3307
blaszorrilla <blassalvador03@gmail.com>
2023-09-12 15:21:20 -0400

    Entradas por teclado y operaciones aritméticas

---
+++ ejercicios modulo 1/ejercicio4.py
@@ -0,0 +1,12 @@
+num1 = int(input("Introduce un numero entero: "))
+num2 = int(input("Introduce otro numero: "))
+
+suma = num1+num2
+resta = num1-num2
+multiplicacion = num1*num2
+division = num1/num2
+
+print(f"La suma de {num1} y {num2} es igual a:",suma)
+print(f"La resta de {num1} y {num2} es igual a:",resta)
+print(f"La multiplicacion de {num1} y {num2} es igual a:",multiplicacion)
+print(f"La division de {num1} y {num2} es igual a:",division)
